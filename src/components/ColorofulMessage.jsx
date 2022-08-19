081b4a296bfbb892e78653f016e0f128d1bf8106
kazusukegram <111216163+kazusukegram@users.noreply.github.com>
2022-08-19 16:33:18 +0900

    カウントアップ 途中経過

---
+++ src/components/ColorofulMessage.jsx
@@ -0,0 +1,11 @@
+const ColorfulMessage = (props) => {
+  const { color, children } = props;
+  const contentStyle = {
+    color,
+    fontsize: "18px"
+  };
+
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColorfulMessage;
