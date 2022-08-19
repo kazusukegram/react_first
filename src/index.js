46aaea3572cc39935e8cf8dd8a0fc064297c5489
kazusukegram <111216163+kazusukegram@users.noreply.github.com>
2022-08-19 15:18:06 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>こんにちは!</h1>
+      <p>お元気ですか？</p>
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
